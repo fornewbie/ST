b2b75e8846295b67cd5f37f71623897542b52f62
fornewbie <161813376+fornewbie@users.noreply.github.com>
2024-04-06 20:36:45 +0700

    firebase init

---
+++ public/js/index.js
@@ -0,0 +1,6 @@
+// const home = document.createElement("div");
+// home.id = "home"
+
+// // const content = document.getElementById("content")
+// //  home.innerHTML = "content home"
+// //  content.appendChild(home)
